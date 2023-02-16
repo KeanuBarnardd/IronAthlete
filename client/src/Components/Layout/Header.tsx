88fe211aeb913ae05f9290e4cc8da2a1849d3d3b
Keanu Barnard <keanu.barnardd@gmail.com>
2023-02-16 15:51:02 +1100

    Fetched MenuItems Data from API

--- client/src/Components/Layout/Header.tsx
+++ client/src/Components/Layout/Header.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React from "react";
+
+let logo = require("../../Assets/Images/mango.png");
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+      <div className="container-fluid">
+        <img src={logo} style={{ height: "40px" }} className="m-1" />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item ">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only"></span>
+              </a>
+            </li>
+     
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Admin Panel
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+         
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- client/src/Containers/App.tsx
+++ client/src/Containers/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
-import {Header, Footer} from "../Components/Layout/index";
+import { Header, Footer } from "../Components/Layout/index";
+import { useState, useEffect } from "react";
+import { menuItemModel } from "../Interfaces";
 
 function App() {
+  // Creates an Array of MenuItemModels...
+  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+
+  useEffect(() => {
+    //Get our Menu Item Data
+    fetch("https://freshdirectapi.azurewebsites.net/api/MenuItem")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMenuItems(data.result);
+      });
+  }, []);
+
   return (
     <div>
       <Header /> Main Component <Footer />
