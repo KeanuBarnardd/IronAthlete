77bd710e8b3ea42e7bf6ff5e5136e3bb2f7a4ecd
Keanu Barnard <keanu.barnardd@gmail.com>
2023-02-21 09:51:37 +1100

    Added login validation

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -1,21 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLoginUserMutation } from "../Apis/authApi";
+import { inputHelper } from "../Helper";
+import { apiResponse } from "../Interfaces";
 
 export default function Login() {
+  const [error, setError] = useState("");
+  const [loginUser] = useLoginUserMutation();
+  const [loading, setLoading] = useState(false);
+  const [userInput, setUserInput] = useState({
+    userName: "",
+    password: "",
+  });
+
+  const handleUserInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const tempData = inputHelper(e, userInput);
+    setUserInput(tempData);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    const response: apiResponse = await loginUser({
+      userName: userInput.userName,
+      password: userInput.password,
+    });
+
+    if (response.data) {
+      console.log(response.data);
+    } else if (response.error) {
+      console.log(response.error);
+      setError(response.error.data.errorsMessages[0]);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <div className="container text-center">
-      <form method="post">
+      <form method="post" onSubmit={handleSubmit}>
         <h1 className="mt-5">Login</h1>
         <div className="mt-5">
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
-            <input type="text" className="form-control" placeholder="Enter Username" required />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Username"
+              required
+              name="userName"
+              value={userInput.userName}
+              onChange={handleUserInput}
+            />
           </div>
 
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
-            <input type="password" className="form-control" placeholder="Enter Password" required />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter Password"
+              required
+              name="password"
+              value={userInput.password}
+              onChange={handleUserInput}
+            />
           </div>
         </div>
 
         <div className="mt-2">
+          {error && <p className="text-danger">{error}</p>}
           <button type="submit" className="btn btn-success" style={{ width: "200px" }}>
             Login
           </button>
