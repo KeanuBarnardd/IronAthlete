b669bc67a70407cb0a99c85ba1f20c96ed41869a
Keanu Barnard <keanu.barnardd@gmail.com>
2023-02-20 15:27:05 +1100

    Added Pickup details form to shopping cart

--- client/src/Components/Layout/Page/Cart/CartPickUpDetails.tsx
+++ client/src/Components/Layout/Page/Cart/CartPickUpDetails.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
-
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { cartItemModel } from "../../../../Interfaces";
+import { RootState } from "../../../../Storage/Redux/store";
 
 export default function CartPickUpDetails() {
+  const shoppingCartFromStore: cartItemModel[] = useSelector(
+    (state: RootState) => state.shoppingCartStore.cartItems ?? []
+  );
+
+  let grandTotal = 0;
+  let totalItems = 0;
+
+  shoppingCartFromStore?.map((cartItem: cartItemModel) => {
+    totalItems += cartItem.quantity ?? 0;
+    grandTotal += (cartItem.menuItem?.price ?? 0) * (cartItem.quantity ?? 0);
+    return null;
+  });
+
   return (
     <div className="border pb-5 pt-3">
       <h1 style={{ fontWeight: "300" }} className="text-center text-success">
@@ -12,13 +26,7 @@ export default function CartPickUpDetails() {
       <form className="col-10 mx-auto">
         <div className="form-group mt-3">
           Pickup Name
-          <input
-            type="text"
-            className="form-control"
-            placeholder="name..."
-            name="name"
-            required
-          />
+          <input type="text" className="form-control" placeholder="name..." name="name" required />
         </div>
         <div className="form-group mt-3">
           Pickup Email
@@ -43,17 +51,14 @@ export default function CartPickUpDetails() {
         </div>
         <div className="form-group mt-3">
           <div className="card p-3" style={{ background: "ghostwhite" }}>
-            <h5>Grand Total : $10</h5>
-            <h5>No of items : 5</h5>
+            <h5>Grand Total : ${`${grandTotal.toFixed(2)}`}</h5>
+            <h5>No of items : {`${totalItems}`}</h5>
           </div>
         </div>
-        <button
-          type="submit"
-          className="btn btn-lg btn-success form-control mt-3"
-        >
+        <button type="submit" className="btn btn-lg btn-success form-control mt-3">
           Looks Good? Place Order!
         </button>
       </form>
     </div>
-  )
+  );
 }
