0dc36b95526be5bf9c787d666a4ac65f558be8d9
Keanu Barnard <keanu.barnardd@gmail.com>
2023-02-24 22:27:00 +1100

    Added Demo login functionality

--- frontend/src/Components/Layout/AuthFormContainer/DemoLogin.tsx
+++ frontend/src/Components/Layout/AuthFormContainer/DemoLogin.tsx
@@ -1,5 +1,10 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useLoginUserMutation } from "../../../Apis/authApi";
+import { apiResponse, userModel } from "./../../../Interfaces";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setLoggedInUser } from "../../../Storage/Redux/userAuthSlice";
+import jwt_decode from "jwt-decode";
 
 // Demo Login Details
 /*
@@ -7,19 +12,36 @@ import React from "react";
   Name: Demo User
   Password: demo123
   Role: Admin
-  
 */
 
-
-
 export default function DemoLogin() {
+  const [loginUser] = useLoginUserMutation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    const response: apiResponse = await loginUser({
+      userName: "demoUser@gmail.com",
+      password: "demo123",
+    });
+
+    if (response.data) {
+      const { token } = response.data.result;
+      const { fullName, id, email, role }: userModel = jwt_decode(token);
+      localStorage.setItem("token", token);
+      dispatch(setLoggedInUser({ fullName, id, email, role }));
+      navigate("/");
+    } else if (response.error) {
+      setError(response.error.data.errorMessages[0]);
+      console.error("Our demo user does not exist or username password is different please check.");
+    }
+    setLoading(false);
+  };
+
   return (
-    <button
-      className="btn btn-outline"
-      onClick={() => {
-        console.log("Demo Login Button");
-      }}
-    >
+    <button className="btn btn-outline" onClick={handleSubmit}>
       Demo Login
     </button>
   );
