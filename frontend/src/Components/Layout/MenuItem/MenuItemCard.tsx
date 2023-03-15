ed0197033775934f42dd14d4115f829f47db8fc4
Keanu Barnard <keanu.barnardd@gmail.com>
2023-03-15 22:37:50 +1100

    Added menuitem styling + functionality

--- frontend/src/Components/Layout/MenuItem/MenuItemCard.tsx
+++ frontend/src/Components/Layout/MenuItem/MenuItemCard.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+
+// Functionality
 import { menuItemModel } from "../../../Interfaces";
 import { useNavigate } from "react-router-dom";
 
+// Styling
+import "./MenuItemCard.scss";
 export default function MenuItemCard(menuItem: menuItemModel) {
   const navigate = useNavigate();
 
@@ -12,15 +16,36 @@ export default function MenuItemCard(menuItem: menuItemModel) {
         className="img__container"
         style={{ backgroundImage: `url(${menuItem.image})` }}
       >
-        <p>
-          <span>{menuItem.specialTag}</span>
-        </p>
+        {menuItem.specialTag ? (
+          <p>
+            <span>
+              <i className="bi bi-star-fill"></i>
+              {menuItem.specialTag}
+            </span>
+          </p>
+        ) : (
+          <></>
+        )}
       </div>
-      <h2>{menuItem.name}</h2>
-      <p>
-        <span>{menuItem.category}</span>
-      </p>
-      <p className="p-text">{menuItem.description}</p>
+      <div className="menu-item__content">
+        <h2>{menuItem.name}</h2>
+        <div className="row">
+          <p>
+            <span>{menuItem.category}</span>
+          </p>
+          <p>
+            <span>{menuItem.category}</span>
+          </p>
+        </div>
+
+        <p className="p-text">{menuItem.description}</p>
+        <div className="row" style={{alignItems:"center", justifyContent:"space-between"}}>
+          <button className="btn btn-menu-item">
+            Add to cart
+          </button>
+          <h2 className="menu-item-price">${menuItem.price}</h2>
+        </div>
+      </div>
     </div>
   );
 }
