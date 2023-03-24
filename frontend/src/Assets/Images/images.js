2447d4601a2fd7eef12ca7dca14ef637f725f7b0
Keanu Barnard <keanu.barnardd@gmail.com>
2023-03-24 21:41:46 +1100

    Add landing catalogue
    Added home page content - catalogue component.

--- frontend/src/Assets/Images/images.js
+++ frontend/src/Assets/Images/images.js
@@ -3,11 +3,24 @@ import ironAtheleteLogo from "./Logo/IronAthelete_Logo.png";
 import headerImg1 from "./Header_Images/headerImg1.jpg";
 import headerImg2 from "./Header_Images/headerImg2.jpg";
 import headerImg3 from "./Header_Images/headerImg3.jpg";
+import catalogue1 from "./Catalogue/catalogue1.jpg";
+import catalouge2 from "./Catalogue/catalogue2.jpg";
+import catalouge3 from "./Catalogue/catalogue3.jpg";
+import cat_img_1 from "./Catalogue/cataimg1.png";
+import cat_img_2 from "./Catalogue/cataimg2.png";
+import cat_img_3 from "./Catalogue/cataimg3.png";
+import cat_img_4 from "./Catalogue/cataimg4.jpg";
 
 export default {
-  // New images
   ironAtheleteLogo,
   headerImg1,
   headerImg2,
   headerImg3,
+  catalogue1,
+  catalouge2,
+  catalouge3,
+  cat_img_1,
+  cat_img_2,
+  cat_img_3,
+  cat_img_4,
 };
--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Header } from "../../Components/Page/Common";
 import { MenuItemList } from "../../Components/Page/Home";
 import { ItemSlider } from "../../Components/Layout";
-
+import { images } from "../../Assets/Images";
 import "./Home.scss";
 
 function Home() {
@@ -10,8 +10,63 @@ function Home() {
     <>
       <Header />;
       <div className="app__flex">
-        <div className="app__container-width app__container">
+        <div className="app__container-width">
           
+        </div>
+      </div>
+      <div className="app__flex">
+        <div className="app__container-width app__container catalogue__parent-container">
+          <div className="row catalogue__container">
+            <div className="catalogue__container-tall">
+              <span className="sales__tag">SALE 15% OFF</span>
+              <div
+                className="catalogue__container-img"
+                style={{ backgroundImage: `url(${images.cat_img_4})` }}
+              ></div>
+
+              <div className="col">
+                <h1>Heavy dumbell set weights light cruiser brand</h1>
+
+                <div className="row__price">
+                  <h2>$149.99</h2>
+                  <h3>$189.99</h3>
+                </div>
+                <p className="p-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni quibusdam
+                  non ab doloribus atque eius a molestias eos perferendis aperiam esse dolorem,
+                  fugit rem, neque vero deleniti deserunt porro.
+                </p>
+                <div className="row">
+                  <button className="btn btn__accent">Add to cart</button>
+                  <button className="btn btn__outline-dark">View Product</button>
+                </div>
+                <p className="terms">Offer ends 20/06/2023. All terms & conditions apply</p>
+              </div>
+            </div>
+            <div className="col catalogue__container- " style={{ gap: "15px" }}>
+              <div
+                className="col catalogue__container-sale"
+                style={{ backgroundImage: `url(${images.catalouge3})` }}
+              >
+                <h1>30%</h1>
+                <h2>Off All Selected Protein Powders and Gym Wear if you buy today</h2>
+              </div>
+              <div className="catalogue__grid">
+                <div className="catalogue__card">
+                  <h2>Hand weights</h2>
+                  <img src={images.cat_img_1} alt="" />
+                </div>
+                <div className="catalogue__card">
+                  <h2>Safety gear</h2>
+                  <img src={images.cat_img_2} alt="" />
+                </div>
+                <div className="catalogue__card">
+                  <h2>Protein</h2>
+                  <img src={images.cat_img_3} alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
