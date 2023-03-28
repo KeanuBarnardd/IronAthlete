c1c21a8888c052f5fd5c4eb5afe32f19584fd33b
Keanu Barnard <keanu.barnardd@gmail.com>
2023-03-28 16:56:12 +1100

    Added purchase function to cards

--- frontend/src/Components/Layout/ProductCard/ProductCard.tsx
+++ frontend/src/Components/Layout/ProductCard/ProductCard.tsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { images } from "../../../Assets/Images";
-import { menuItemModel } from "../../../Interfaces";
+import { apiResponse, menuItemModel } from "../../../Interfaces";
+
+import { useNavigate } from "react-router-dom";
 import "./ProductCard.scss";
+import { useUpdateShoppingCartMutation } from "../../../Apis/shoppingCartApi";
+import { toastNotify } from "../../../Helper";
+import userModel from "./../../../Interfaces/userModel";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../Storage/Redux/store";
 
 export default function ProductCard(props: menuItemModel) {
+  const navigate = useNavigate();
+
+  const [isAddingToCart, setIsAddingCart] = useState<boolean>(false);
+  const [updateShoppingCart] = useUpdateShoppingCartMutation();
+  const userData: userModel = useSelector((state: RootState) => state.userAuthStore);
+
+  const handleAddToCart = async (menuItemId: number) => {
+    if (!userData.id) {
+      navigate("/login");
+      return;
+    }
+    setIsAddingCart(true);
+
+    const response: apiResponse = await updateShoppingCart({
+      menuItemId: menuItemId,
+      updateQuantityBy: 1,
+      userId: userData.id,
+    });
+
+    if (response.data && response.data.isSuccess) {
+      toastNotify("Item added to cart successfully! ");
+    }
+
+    setIsAddingCart(false);
+  };
+
   return (
     <div className="product__card-container">
       <div
+        onClick={() => navigate(`/menuItemDetails/${props.id}`)}
         className="product__img-container"
         style={{ backgroundImage: `url(${props.image})` }}
       >
@@ -21,10 +55,10 @@ export default function ProductCard(props: menuItemModel) {
           <p className="product__price">${props.price.toFixed(2)}</p>
         </div>
 
-        <p className="p-text">
-         {props.description}
-        </p>
-        <button  className="btn btn__accent">Add to cart</button>
+        <p className="p-text">{props.description}</p>
+        <button onClick={() => handleAddToCart(props.id)} className="btn btn__accent">
+          Add to cart
+        </button>
       </div>
     </div>
   );
